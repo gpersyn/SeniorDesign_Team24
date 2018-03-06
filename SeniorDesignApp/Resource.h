de13e4aeb48a3f9024b8a181caecd0143d9146f1
Garrett Persyn <garrettpersyn@gmail.com>
2018-03-06 14:51:41 -0600

    Test2

--- SeniorDesignApp/Resource.h
+++ SeniorDesignApp/Resource.h
@@ -7,9 +7,9 @@
 #define IDR_MAINFRAME                   128
 #define IDR_SeniorDesignAppTYPE         130
 #define IDD_PRIMEFORM                   310
-#define IDC_LIST3                       1010
-#define IDC_RADIO1                      1011
 #define IDC_EDIT2                       1012
+#define IDC_BUTTON1                     1013
+#define IDC_MONTHCALENDAR2              1014
 
 // Next default values for new objects
 // 
@@ -17,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        311
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_CONTROL_VALUE         1015
 #define _APS_NEXT_SYMED_VALUE           311
 #endif
 #endif
